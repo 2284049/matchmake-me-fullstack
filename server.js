3b96b96d65d48c11056fbdcec8566a0a678e6e47
2284049 <70497897+2284049@users.noreply.github.com>
2020-11-20 10:40:30 -0800

    add express route

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-require("dotenv").config();
-const mysql = require("mysql");
-const selectUser = require("./queries/selectUser");
-const { toJson, toSafeParse } = require("./utils/helpers");
+const express = require("express");
+const app = express();
 
-const connection = mysql.createConnection({
-   host: process.env.RDS_HOST,
-   user: process.env.RDS_USER,
-   password: process.env.RDS_PASSWORD,
-   database: "matchmake_me_app",
+app.use("/api/v1/users", require("./api/v1/users"));
+app.get("/", (req, res) => {
+   res.send("Hello World!");
 });
 
-connection.connect();
+const port = process.env.PORT || 3046;
 
-connection.query(
-   selectUser("megan@gmail.com", "b3d4f347d71bc0d2a6af2cfc5fd81bb8"),
-   (err, res) => {
-      if (err) {
-         console.log(err);
-      } else {
-         const users = toSafeParse(toJson(res));
-         console.log(users);
-      }
-   }
-);
-
-connection.end();
+app.listen(port, () => {
+   console.log(`Server running at http://localhost:${port}`);
+});
