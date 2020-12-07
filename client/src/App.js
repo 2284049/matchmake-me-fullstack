9550cbd072657e77dae68f5a824000763aecbf95
2284049 <70497897+2284049@users.noreply.github.com>
2020-12-07 13:21:40 -0800

    add temp auth using json web token

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,53 @@ import Questionnaire from "./components/pages/Questionnaire";
 import Matches from "./components/pages/MatchesPage";
 import Match from "./components/pages/MatchPage";
 import NotFound from "./components/pages/NotFound";
+import jwtDecode from "jwt-decode";
+import store from "./store/store";
+import actions from "./store/actions";
+
+const authToken = localStorage.authToken;
+if (authToken) {
+   // if the authToken is not expired
+   const currentTimeInSeconds = Date.now() / 1000;
+   const user = jwtDecode(authToken);
+   if (currentTimeInSeconds > user.exp) {
+      console.log("expired token");
+      store.dispatch({
+         type: actions.UPDATE_CURRENT_USER,
+         payload: {},
+      });
+      // remove the currentUser from the global store
+   } else {
+      console.log("valid token");
+      // store user in global state / redux store (currentUser)
+      store.dispatch({
+         type: actions.UPDATE_CURRENT_USER,
+         payload: user,
+      });
+      const currentURL = window.location.pathname;
+      if (currentURL === "/") {
+         window.location.href = "/questionnaire";
+         // redirect to next page
+      }
+   }
+   // set authorization headers
+} else {
+   console.log("no token");
+}
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/sign-in" component={SignInPage} />
-        <Route exact path="/questionnaire" component={Questionnaire} />
-        <Route exact path="/matches" component={Matches} />
-        <Route exact path="/match" component={Match} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  );
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/sign-in" component={SignInPage} />
+            <Route exact path="/questionnaire" component={Questionnaire} />
+            <Route exact path="/matches" component={Matches} />
+            <Route exact path="/match" component={Match} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
